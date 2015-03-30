f135018d1b9ed10efacc3221c7d36d035fb97747
Arkadiusz Kotarba <arek.kotarba@student.uj.edu.pl>
2015-03-30 17:47:34 +0200

    - added Sushi's cube scripts;

---
+++ Assets/Scripts/Utils/GameObjectHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace NeonShooter.Utils
+{
+    public static class GameObjectHelper
+    {
+        public static void SetParentDontMessUpCoords(GameObject childToBe, GameObject parentToBe)
+        {
+            var oldTranslation = childToBe.transform.localPosition;
+            var oldRotation = childToBe.transform.localRotation;
+            var oldScale = childToBe.transform.localScale;
+
+            childToBe.transform.parent = parentToBe.transform;
+
+            childToBe.transform.localPosition = oldTranslation;
+            childToBe.transform.localRotation = oldRotation;
+            childToBe.transform.localScale = oldScale;
+        }
+    }
+}
