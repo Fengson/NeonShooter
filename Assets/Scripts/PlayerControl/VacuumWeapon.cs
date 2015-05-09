8269a4bace2a6cb37e707c239deffa841a06828e
Grzegorz Poręba <grzegorz.poreba@gmail.com>
2015-05-10 01:41:00 +0200

    Fix after merge.

--- Assets/Scripts/PlayerControl/VacuumWeapon.cs
+++ Assets/Scripts/PlayerControl/VacuumWeapon.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class VacuumWeapon : Weapon {
 
 
-	public VacuumWeapon() : base(50, 0, (float)(4.0/18.0*Mathf.PI), 10)
+	public VacuumWeapon() : base(50, 10, (float)(4.0/18.0*Mathf.PI), 10)
 	{
 	}
 
-	public override void shoot(Player shooter, IPlayer target)
-	{
+   	public override void shoot(Player shooter, IPlayer target)
+   	{
     	RaycastHit hitInfo;
-		Vector3 heading = (target.Position[null] - shooter.Position[null]).normalized;
-		double angle_cos = Vector3.Dot(heading, shooter.Direction[null].normalized);
-		if(angle_cos > this.ConeAngleCos)
-		{
-			if(Physics.Raycast(shooter.Position[null], heading, out hitInfo, this.Reach))
-			{
-				shooter.enemyShot(this, hitInfo.collider, this.Damage);
-			}
-		}
-	}
+   		Vector3 heading = (target.Position[null] - shooter.Position[null]).normalized;
+   		double angle_cos = Vector3.Dot(heading, shooter.Direction[null].normalized);
+   		if(angle_cos > this.ConeAngleCos)
+   		{
+   			if(Physics.Raycast(shooter.Position[null], heading, out hitInfo))
+   			{
+   				if(hitInfo.distance <= this.Reach){
+					shooter.enemyShot(this, hitInfo.collider, this.Damage);
+   				}
+   			}
+   		}
+   	}
 
 	public override string getWeaponName() {
 		return "Vacuum";
