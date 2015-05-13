6191cf57aad0327c191e8c7643be90cf731e24f5
Arkadiusz Kotarba <arek.kotarba@student.uj.edu.pl>
2015-05-13 21:37:55 +0200

    - added checking hit agaist enemy list; (not tested)

--- Assets/Scripts/PlayerControl/VacuumWeapon.cs
+++ Assets/Scripts/PlayerControl/VacuumWeapon.cs
@@ -2,51 +2,27 @@
 using NeonShooter.PlayerControl;
 using UnityEngine;
 
-public class VacuumWeapon : Weapon {
-
-
-	public VacuumWeapon() : base(50, 10, (float)(4.0/18.0*Mathf.PI), 0)
-	{
-	}
+public class VacuumWeapon : Weapon
+{
+	public VacuumWeapon() : base(50, 10, (float)(4.0/18.0*Mathf.PI), 0)	{}
 
    	public override void shoot(Player shooter, int costPayed)
    	{
-   	   //TODO change this Arek when we get to know how to get IPlayer array
-    	Collider[] hitColliders = Physics.OverlapSphere(shooter.Position[null], Reach);
-		int k = 0;
-        while (k < hitColliders.Length) {
-            if(hitColliders[k].name=="Plane" || hitColliders[k].name=="FPSController") {
-            	k++;
-            	continue;
-            }
-        	Vector3 heading = (hitColliders[k].bounds.center - shooter.Position[null]).normalized;
+		foreach(GameObject target in appwarp.enemies)
+		{
+			Vector3 heading = (target.transform.position - shooter.Position[null]).normalized;
         	double angle_cos = Vector3.Dot(heading, shooter.Direction[null].normalized);
         	if(angle_cos > this.ConeAngleCos)
-           		{
-				shooter.enemyShot(this, hitColliders[k], this.Damage, costPayed);
-            	}
-            k++;
+           	{
+				RaycastHit hit;
+				if(Physics.Raycast(shooter.Position[null], heading, out hit, this.Reach))
+				{
+					shooter.enemyShot(this, target.GetComponent<Collider>(), this.Damage, costPayed);
+				}
+            }
    		}
    	}
-
-/*
-OLD METHOD
-   	public override void shoot(Player shooter, IPlayer target, int costPayed)
-   	{
-    	RaycastHit hitInfo;
-   		Vector3 heading = (target.Position[null] - shooter.Position[null]).normalized;
-   		double angle_cos = Vector3.Dot(heading, shooter.Direction[null].normalized);
-   		if(angle_cos > this.ConeAngleCos)
-   		{
-   			if(Physics.Raycast(shooter.Position[null], heading, out hitInfo))
-   			{
-   				if(hitInfo.distance <= this.Reach){
-					shooter.enemyShot(this, hitInfo.collider, this.Damage, costPayed);
-   				}
-   			}
-   		}
-   	}
- */
+	
    	/**
    	method redundant for this specific weapon, suction speed would be more useful, as the distance will vary
    	*/
